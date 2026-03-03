0856193bb87d4cd50f830d1e0ecc80953abd94cb
Ahmed Faiyaz <217755415+ahmed-faiyaz-bss@users.noreply.github.com>
2026-03-03 15:13:35 +0600

    Update install-els.blade.php

--- views/ssh/install-els.blade.php
+++ views/ssh/install-els.blade.php
@@ -2,5 +2,10 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get install -y alt-php{{ $version }
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
+if [ -f /opt/alt/php{{ $version }}/etc/php-fpm.d/www.conf ]; then
+    sudo sed -i 's/apache/{{ $user }}/g' /opt/alt/php{{ $version }}/etc/php-fpm.d/www.conf
+    sudo sed -i 's/nobody/{{ $user }}/g' /opt/alt/php{{ $version }}/etc/php-fpm.d/www.conf
+fi
+
 sudo systemctl enable alt-php{{ $version }}-fpm
 sudo systemctl start alt-php{{ $version }}-fpm

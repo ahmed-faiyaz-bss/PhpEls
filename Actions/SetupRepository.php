dac957c62bc19e7fd358dbe77c4b89f9fb55fd74
Ahmed Faiyaz <217755415+ahmed-faiyaz-bss@users.noreply.github.com>
2026-03-04 11:46:34 +0600

    Update SetupRepository.php

--- Actions/SetupRepository.php
+++ Actions/SetupRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Vito\Plugins\Tuxcare\AhmedFaiyazBss\Actions;
+namespace App\Vito\Plugins\AhmedFaiyazBss\PhpEls\Actions;
 
 use App\DTOs\DynamicField;
 use App\DTOs\DynamicForm;
@@ -22,26 +22,11 @@ class SetupRepository extends Action
 
     public function form(): DynamicForm
     {
-        if ($this->isRepoInstalled()) {
-            return DynamicForm::make([
-                DynamicField::make('alert')
-                    ->alert()
-                    ->options(['type' => 'success'])
-                    ->description('PHP ELS repository is already set up on this server. You can install PHP ELS versions from the Services page.'),
-                DynamicField::make('license_key')
-                    ->text()
-                    ->label('License Key')
-                    ->placeholder('XXX-XXXXXXXXXXXX')
-                    ->description('Re-enter your license key only if you need to re-setup the repository'),
-            ]);
-        }
-
         return DynamicForm::make([
             DynamicField::make('alert')
                 ->alert()
-                ->options(['type' => 'info'])
                 ->link('TuxCare PHP ELS Docs', 'https://docs.tuxcare.com/els-for-runtimes/php/')
-                ->description('Enter your TuxCare license key to set up the PHP ELS repository. This only needs to be done once per server.'),
+                ->description('Enter your TuxCare license key to set up the PHP ELS repository. If the repository is already set up, this will re-run the setup.'),
             DynamicField::make('license_key')
                 ->text()
                 ->label('License Key')
@@ -64,18 +49,5 @@ class SetupRepository extends Action
         );
 
         $request->session()->flash('success', 'PHP ELS repository set up successfully!');
-    }
-
-    private function isRepoInstalled(): bool
-    {
-        try {
-            $result = $this->server->ssh()->exec(
-                'ls /etc/apt/sources.list.d/*alt-php*.list 2>/dev/null && echo "REPO_EXISTS" || echo "REPO_MISSING"'
-            );
-
-            return str_contains($result, 'REPO_EXISTS');
-        } catch (\Throwable) {
-            return false;
-        }
     }
 }
